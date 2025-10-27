5cbad7c59a793471f15d2ec8e4cd5e668eb77840
Kearlan <lokeyo@metapocrates.com>
2025-10-27 13:35:41 -0700

    Mark athlete OG image route as dynamic

--- src/app/(public)/athletes/[username]/opengraph-image.tsx
+++ src/app/(public)/athletes/[username]/opengraph-image.tsx
@@ -2,6 +2,8 @@
 import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
